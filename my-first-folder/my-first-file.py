14d15987e07a2710342b4e8f4da1fd78975de43d
Joseph <joseph123kimmy@gmail.com>
2022-01-12 18:54:16 -0500

    if statement practice

---
+++ my-first-folder/my-first-file.py
@@ -0,0 +1,14 @@
+x=1
+print(x)
+if x>=10:
+    print("this number greater than 10")
+    if x>=50:
+        print("This number is greater or equal to 50")
+elif x<10 and x>5:
+    print("this number is between 5 and 10")
+
+elif x<=5:
+    print("if it's less than or equal to 5")
+    if x==1:
+        print("this number is equal to 1")
+
